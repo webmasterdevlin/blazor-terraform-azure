77fb4d3775908b11dfd872d8ffd601d59b61f349
Devlin <webmasterdevlin@gmail.com>
2020-02-21 09:41:50 +0100

    renamed container and homepage

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
    storage_account_name   = "__terraformstorageaccount__"
-   container_name         = "terraform"
+   container_name         = "terraformcontainer"
    key                    = "terraform.tfstate"
 	 access_key             = "__storagekey__"
   }
